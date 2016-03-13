3e5621b0c8ae23f9168fa53c1929f054596248b2
Mag <mag@magwas.rulez.org>
2016-03-14 00:15:25 +0100

    shippable integration

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -xe
-export PYTHONPATH=src
+export PYTHONPATH=src:tests:cryptoserver
+export SOFTHSM_CONF=tests/softhsm.conf
 python -m unittest discover -s tests -p '*.py'
+python -m unittest discover -s integrationtests -p '*.py'
 wget -S --no-check-certificate --post-file=tests/query.data --header "Content-type: application/x-www-form-urlencoded" https://localhost:8890/anchor -O tmp/reply
 diff -bu testserver/expectedreply tmp/reply 
 
